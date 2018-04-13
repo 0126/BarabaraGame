408038386d15a0e18d5cb0f4aa99e5597e0e2ee0
Yoshiki Kishimoto <0126yoshiki@icloud.com>
2018-04-14 02:03:58 +0900

    Initial Commit

---
+++ BarabaraGame/Barabara素材/RankinnguViewController.swift
@@ -0,0 +1,36 @@
+//
+//  RankinnguViewController.swift
+//  BarabaraGame
+//
+//  Created by Yoshiki Kishimoto on 2018/04/11.
+//  Copyright © 2018年 Yoshiki Kishimoto. All rights reserved.
+//
+
+import UIKit
+
+class RankinnguViewController: UIViewController {
+    
+    @IBAction func toTop() {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+
+        // Do any additional setup after loadin
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+            }
+        }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
